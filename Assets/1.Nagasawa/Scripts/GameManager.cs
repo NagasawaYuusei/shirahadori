89204320ad869910c6679f674f0c024571c6070f
NagasawaYusei <curga3013@gmail.com>
2022-06-05 14:13:17 +0900

    [Update] GameManager更新

--- Assets/1.Nagasawa/Scripts/GameManager.cs
+++ Assets/1.Nagasawa/Scripts/GameManager.cs
@@ -8,10 +8,11 @@ public class GameManager : MonoBehaviour
     [Tooltip("インスタンスを取得しやすいように")] public static GameManager Instance;
     [Tooltip("false:Player1 true:Player2 各勝敗")] bool _isWinPlayer;
     [Tooltip("各プレイヤーのアクション")] bool[] _isAction = new bool[2];
-    //[Tooltip("")] 
-    [Tooltip("先行か後攻か")] PlayerState[] _playerModes;
+    [Tooltip("")] float _attackTime; 
+    [Tooltip("先行か後攻か")] PlayerMode[] _playerModes;
     [Tooltip("攻撃するプレイヤーの番号")] int _attackPlayerNum;
     [Tooltip("防御するプレイヤーの番号")] int _protectPlayerNum;
+    float _time;
     bool a;
 
     void Awake()
@@ -49,9 +50,42 @@ public class GameManager : MonoBehaviour
         _isAction[1] = false;
     }
 
+    /// <summary>
+    /// ゲーム処理
+    /// </summary>
     public void WhichPlayerWin()
     {
-        //if()
+        if(_isAction[_attackPlayerNum])
+        {
+            _time += Time.deltaTime;
+            if(_isAction[_protectPlayerNum])
+            {
+                _isWinPlayer = false;
+            }
+            
+            if(_time >= _attackTime)
+            {
+                _isWinPlayer = true;
+            }
+        }
+    }
+
+    /// <summary>
+    /// プレイヤーの攻守を交代
+    /// </summary>
+    public void ChangePlayerMode()
+    {
+        for(int i = 0; i < 2; i++)
+        {
+            if(_playerModes[i] == PlayerMode.Attack)
+            {
+                _playerModes[i] = PlayerMode.Protect;
+            }
+            else
+            {
+                _playerModes[i] = PlayerMode.Attack;
+            }
+        }
     }
 
     /// <summary>
@@ -59,11 +93,14 @@ public class GameManager : MonoBehaviour
     /// </summary>
     public void PlayerModeNumSet()
     {
-        _attackPlayerNum = Array.IndexOf(_playerModes, PlayerState.Attack);
-        _protectPlayerNum = Array.IndexOf(_playerModes, PlayerState.Protect);
+        _attackPlayerNum = Array.IndexOf(_playerModes, PlayerMode.Attack);
+        _protectPlayerNum = Array.IndexOf(_playerModes, PlayerMode.Protect);
     }
 
-    public enum PlayerState
+    /// <summary>
+    /// Playerの攻守
+    /// </summary>
+    public enum PlayerMode
     {
         Attack,
         Protect,
